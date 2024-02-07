2c44fc1bb673aafc92faf3ae3d9ce8db24a7892b
Habu <habu1010+github@gmail.com>
2024-02-08 05:38:07 +0900

    feat: テスト用のコード

--- tests/web.rs
+++ tests/web.rs
@@ -11,3 +11,34 @@ wasm_bindgen_test_configure!(run_in_browser);
 fn pass() {
     assert_eq!(1 + 1, 2);
 }
+
+use wasm_game_of_life::Universe;
+
+#[cfg(test)]
+pub fn input_spaceship() -> Universe {
+    let mut universe = Universe::new();
+    universe.set_width(6);
+    universe.set_height(6);
+    universe.set_cells(&[(1, 2), (2, 3), (3, 1), (3, 2), (3, 3)]);
+    universe
+}
+
+#[cfg(test)]
+pub fn expected_spaceship() -> Universe {
+    let mut universe = Universe::new();
+    universe.set_width(6);
+    universe.set_height(6);
+    universe.set_cells(&[(2, 1), (2, 3), (3, 2), (3, 3), (4, 2)]);
+    universe
+}
+
+#[wasm_bindgen_test]
+pub fn test_tick() {
+    // Create a smaller universe to test the tick function
+    let mut input_universe = input_spaceship();
+    let expected_universe = expected_spaceship();
+
+    // Call `tick` and then check the cells of the universe
+    input_universe.tick();
+    assert_eq!(&input_universe.get_cells(), &expected_universe.get_cells());
+}
